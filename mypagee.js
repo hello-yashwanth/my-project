d6c303981a0ac6863a3058a93ef6ef4f798ca2fa
YASWANTH <yaswanthsiddanathi@gmail.com>
2024-06-10 13:08:08 +0530

    Add files via upload

---
+++ mypagee.js
@@ -0,0 +1,10 @@
+ScrollReveal({
+   reset: true,
+   distance:'80px',
+   duration: 2000,
+   delay: 300
+  
+});
+ScrollReveal().reveal('.box,.box1,.box2,.box3,.box4,.box5,.boox,  .javabox,.nodebox , .databox2, .face,.whats,.link,.insta,#para,.para1,.para2,#para3 ', { interval:500 });
+
+ScrollReveal().reveal('#skill,.skill1' , { origin: 'top' });
